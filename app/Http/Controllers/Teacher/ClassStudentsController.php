cd3fbb44f2347e835ef35837084feac13918910a
anhkhoahapo <khoand@haposoft.com>
2017-11-17 16:33:30 +0700

    Fix class register & unregister

--- app/Http/Controllers/Teacher/ClassStudentsController.php
+++ app/Http/Controllers/Teacher/ClassStudentsController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Teacher;
 
-use App\Models\SchoolClass;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ClassStudentsController extends Controller
 {
     public function getListStudent($class_id){
-        $class = SchoolClass::with('students')->findOrFail($class_id);
+        $teacher = auth()->guard('teacher')->user();
+
+        $class = $teacher->schoolClasses()->with('students')->findOrFail($class_id);
 
         return view('teacher.class-students', ['class' => $class]);
     }
